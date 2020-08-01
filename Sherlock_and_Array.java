747261baca83cc735612fc87e5b524ba18a99eff
ambatisandeep <61520051+ambatisandeep@users.noreply.github.com>
2020-08-01 23:50:57 +0530

    Sherlock and Array Solution
    https://www.hackerrank.com/challenges/sherlock-and-array/problem

---
+++ Sherlock_and_Array.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    static String balancedSums(List<Integer> arr) {
+           return newMethod(arr.stream().mapToInt(i->i).toArray());
+    }
+      
+static String newMethod(int arr[]) {
+        int sum =0,leftSum=0;
+        for(int i:arr) {
+            sum+=i;
+        }
+        for(int i=0;i<arr.length;i++) {
+            if(leftSum ==(sum-leftSum)) 
+                return "YES";
+            else
+                leftSum +=arr[i];
+        }
+        
+        return "NO";
+    }
+    
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
