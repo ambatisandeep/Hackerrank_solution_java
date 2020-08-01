dca99943e0e3be67cc0114bf2aafebe011eb0ab0
ambatisandeep <61520051+ambatisandeep@users.noreply.github.com>
2020-08-01 23:54:25 +0530

    Sherlock and Array Solution
    Problem Link https://www.hackerrank.com/challenges/sherlock-and-array/problem

--- Sherlock_and_Array.java
+++ Sherlock_and_Array.java
@@ -18,7 +18,7 @@ public class Solution {
 static String newMethod(int arr[]) {
         int sum =0,leftSum=0;
         for(int i:arr) {
-            sum+=i;
+            sum=sum+i;
         }
         for(int i=0;i<arr.length;i++) {
             if(leftSum ==(sum-leftSum)) 
